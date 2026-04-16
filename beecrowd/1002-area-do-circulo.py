75e71e54051fd019b37ed4f04bd4d25391403722
analoisej <20261ctb0100041@estudantes.ifpr.edu.br>
2026-04-16 19:52:51 +0000

    Aula 10 - 1002: area do circulo

---
+++ beecrowd/1002-area-do-circulo.py
@@ -0,0 +1,16 @@
+'''
+Problema:beecrowd | 1002
+Data: 2026.04.06
+Estudante: Ana LOise Jovino Prado
+'''
+#Objetivo: calcular a area da circuferencia com valor de pi
+
+# --- ANÁLISE (LIAC) ---
+#Entrada: variavel: raio
+#Processamento: calcular a area da circuferencia
+#Mostrar a mensagem A= e a resposta
+
+R = float(input())
+PI = 3.14159
+AREA = PI * (R ** 2)
+print(f"A={AREA:.4f}")
