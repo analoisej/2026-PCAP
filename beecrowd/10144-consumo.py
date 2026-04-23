2d461e47d67cf3d75a9b4c126e14b7492b640432
analoisej <20261ctb0100041@estudantes.ifpr.edu.br>
2026-04-23 19:47:17 +0000

    Aula 12 - 10144: consumo

---
+++ beecrowd/10144-consumo.py
@@ -0,0 +1,14 @@
+'''
+Problema: beecrowd | 1014
+Data: 2026.04.23
+Estudante: Ana Loise Jovino Prado
+'''
+#Objetivo: calcular o consumo médio de um automóvel em km/l
+#--- ANALISE (LIAC) ---
+#Entrada: numeros inteiros e em km 
+#Saida: consumo médio em km/l
+
+X = int(input())
+Y = float(input())
+consumo = X / Y
+print(f"{consumo:.3f} km/l")
