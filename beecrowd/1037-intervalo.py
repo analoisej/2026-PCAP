96c9b29d821d76302f54cb902e131036863251ab
analoisej <20261ctb0100041@estudantes.ifpr.edu.br>
2026-05-10 19:43:01 +0000

    Aula 12 - 1037: intervalo

---
+++ beecrowd/1037-intervalo.py
@@ -0,0 +1,19 @@
+'''
+Problema: beecrowd |1037
+Data: 2026.05.10
+Estudante: Ana Loise Jovino Prado
+'''
+#Objetivo: Leia um valor de ponto flutuante com duas casas decimais. Este valor representa um valor monetário.
+#Entrada: O programa deve ler um valor de ponto flutuante com duas casas decimais.
+#Saida: O programa deve mostrar a mensagem "NOTAS:" 
+valor = float(input())
+if 0 <= valor <= 25:
+    print("Intervalo [0,25]")
+elif 25 < valor <= 50:
+    print("Intervalo (25,50]")
+elif 50 < valor <= 75:
+    print("Intervalo (50,75]")
+elif 75 < valor <= 100:
+    print("Intervalo (75,100]")
+else:
+    print("Fora de intervalo")
