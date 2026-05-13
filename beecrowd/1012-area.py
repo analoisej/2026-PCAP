fc9f7ed41d50a1e68fb847980a6daf4eb2ab44ce
analoisej <20261ctb0100041@estudantes.ifpr.edu.br>
2026-05-13 23:44:46 +0000

    Aula 13 - 1013: maior

--- beecrowd/1012-area.py
+++ beecrowd/1012-area.py
@@ -7,7 +7,6 @@ Estudante: Ana Loise Jovino Prado
 #---ANALISE(LIAC)---
 #Entrada: tres valores com um digito após o ponto decimal
 #Saída: deverá conter 5 linhas de dados.
-
 valores = input().split()
 A = float(valores[0])
 B = float(valores[1])
@@ -16,9 +15,10 @@ triangulo = (A * C) / 2
 circulo = 3.14159 * (C ** 2)
 trapezio = ((A + B) * C) / 2
 quadrado = B ** 2
-retangulo = A * B
+retangulo = A * B 
 print(f"TRIANGULO: {triangulo:.3f}")
 print(f"CIRCULO: {circulo:.3f}")
 print(f"TRAPEZIO: {trapezio:.3f}")
 print(f"QUADRADO: {quadrado:.3f}")
 print(f"RETANGULO: {retangulo:.3f}")
+
---
+++ beecrowd/1013-o-maior.py
@@ -0,0 +1,14 @@
+'''
+Problema: beecrowd |1012
+Data: 2026.05.13
+Estudante: Ana Loise Jovino Prado   
+'''
+#Objetivo:Fazer um programa que leia três valores e apresente o maior dos três valores lidos.
+#---ANALISE(liac)---
+#Entrada: três valores inteiros 
+#Saida: imprimir o maior dos três valores seguido da mensagem "eh o maior"
+
+a, b, c = map(int, input().split())
+maiorAB = (a + b + abs(a - b)) // 2
+maiorABC = (maiorAB + c + abs(maiorAB - c)) // 2
+print(f"{maiorABC} eh o maior")
