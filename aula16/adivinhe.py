d712475001b6ddaf30f438e7a2bfbfa01fbc0319
analoisej <20261ctb0100041@estudantes.ifpr.edu.br>
2026-05-28 20:34:42 +0000

    aula16: jogo adivinhe o numero com 3 niveis

--- aula16/adivinhe.py
+++ aula16/adivinhe.py
@@ -14,5 +14,35 @@ def jogar(maximo, chances):
         palpite = int(input("Seu palpite (1 a " + str(maximo) + "): "))
 
         if palpite == numero_secreto:
-            print("Acertou!")
-            
+            print("🎉Acertou!")
+            acertou = True 
+        elif palpite < numero_secreto:
+            print("📈 Muito baixo!")
+        else:
+            print("📉 Muito alto!")
+
+
+        chances = chances -1
+        print("Chances restantes:", chances)
+        
+
+    return acertou
+    
+niveis= [
+    ["Fácil", 10, 3],
+    ["Médio", 100, 5],
+    ["Impossível", 1000, 10],
+]
+
+print("Escolha o nível de dificuldade:")
+print("1 - Fácil       (1 a 10, 3 chances)")
+print("2 - Médio       (1 a 100, 5 chances)")
+print("3 - Impossível  (1 a 1000, 10 chances)")
+opcao = int(input("Digite 1, 2 ou 3: "))
+nivel = niveis[opcao - 1]
+print("Voce escolheu o nível:", nivel[0])
+venceu = jogar(nivel[1], nivel[2])
+if not venceu:
+    print("💀 Fim de jogo! Tente um nível mais facil. 😉")
+
+    
